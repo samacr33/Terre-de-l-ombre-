ab0a179ee1eaa2d8f2de02b7f87560f7823a16d1
samacr33 <sam.prommier@hotmail.fr>
2025-03-24 20:46:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-// script.js de base
-console.log("Script chargé !");
+// Gestion de la sélection de personnage
+function selectCharacter(name, description) {
+    document.getElementById("selected-character").innerText = "Personnage sélectionné : " + name;
+    localStorage.setItem("selectedCharacter", name);
+    document.getElementById("start-game").style.display = "block";
+}
+
+// Redirection vers la prochaine page
+function startGame() {
+    const selectedCharacter = localStorage.getItem("selectedCharacter");
+    if (selectedCharacter) {
+        window.location.href = "jeu.html"; // Crée cette page si elle n'existe pas encore
+    } else {
+        alert("Veuillez sélectionner un personnage avant de commencer !");
+    }
+}

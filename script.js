bdcebf3df9d9efeb10bd4e4661a65e4201ec423c
samacr33 <sam.prommier@hotmail.fr>
2025-03-25 00:14:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 let selectedHero = null;
 
 function selectHero(heroElement) {
-  // Enlever la sélection précédente, si elle existe
   if (selectedHero) {
     selectedHero.classList.remove('selected');
   }
 
-  // Ajouter la classe "selected" à l'élément cliqué
   selectedHero = heroElement;
   selectedHero.classList.add('selected');
 
-  // Activer le bouton de validation
   document.getElementById('confirmButton').disabled = false;
 }
 
 function confirmSelection() {
-  // Afficher une alerte avec le nom du héros choisi
   const heroName = selectedHero.querySelector('p').innerText;
-  alert(`Vous avez choisi ${heroName} !`);
 
-  // Rediriger ou effectuer une autre action après la confirmation
-  // window.location.href = "suivant.html";  // Exemple pour rediriger
+  // Stocker dans le localStorage
+  localStorage.setItem('selectedHero', heroName);
+
+  // Redirection vers la prochaine page
+  window.location.href = 'heros.html'; // à créer ensuite
 }
